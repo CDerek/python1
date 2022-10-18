083c1f8cda0c8fa52d4c780a892710e6e7d6e28d
CDerek <104074461+CDerek@users.noreply.github.com>
2022-10-18 17:07:02 +0100

    Add files via upload

---
+++ chidi.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+age = input( "What is your age ? ")
+height = input("What is your height? ")
+if age <= "30":
+    print("You are not allowed to run this App")
+    print(" Try again when you grow up")
+else:
+    print("My name is "+name)
+    print( "I am " + age + " years old ")
+    print( "My height is " + height + " feet")
+print(name.upper())
